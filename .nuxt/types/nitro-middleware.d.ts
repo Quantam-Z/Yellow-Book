c5159229bf29035e469896fc39de6a56c4f6b5b7
Zahid <rizraizada000@gmail.com>
2025-10-01 23:19:38 +0600

    issue

--- .nuxt/types/nitro-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-export type MiddlewareKey = "auth" | "guest" | "require-auth"
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
-  }
-}
