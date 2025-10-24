b7429e90c8c43d5d92785d0491d0c196e49ba276
Zahid <rizraizada000@gmail.com>
2025-10-24 22:57:15 +0600

    Revert "Merge pull request #84 from Quantam-Z/revert-83-cursor/make-assign-companies-responsive-for-mobile-39f9"
    This reverts commit f32f346d73fb2c88a19d8cd00da2b12af2901643, reversing
    changes made to a081b746cc943936261c0892c23ebc937abdd60d.

--- src/components/SubAdmin/assignCompanies.vue
+++ src/components/SubAdmin/assignCompanies.vue
@@ -88,12 +88,16 @@
             </div>
 
             <div class="pt-2 flex justify-center">
-              <button
+              <span
+                role="button"
+                tabindex="0"
                 @click="viewDetails(company)"
-                class="text-xs font-medium text-yellow-600 hover:text-yellow-700 active:text-yellow-800 transition touch-manipulation underline"
+                @keydown.enter="viewDetails(company)"
+                @keydown.space.prevent="viewDetails(company)"
+                class="inline-flex items-center justify-center px-3 py-1.5 rounded-md bg-blue-50 text-blue-700 text-xs font-medium hover:bg-blue-100 active:bg-blue-200 transition cursor-pointer select-none"
               >
                 Details
-              </button>
+              </span>
             </div>
           </div>
         </div>
